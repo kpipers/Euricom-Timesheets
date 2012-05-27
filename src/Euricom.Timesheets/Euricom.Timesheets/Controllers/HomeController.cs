e25dcfcee781c629e9515f1a3271a18c39e4c506
Jef Claes <jef.claes@gmail.com>
2012-05-27 12:51:05 +0200

    Removed About and Contact views

--- src/Euricom.Timesheets/Euricom.Timesheets/Controllers/HomeController.cs
+++ src/Euricom.Timesheets/Euricom.Timesheets/Controllers/HomeController.cs
@@ -13,20 +13,6 @@ namespace Euricom.Timesheets.Controllers
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
 
             return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your quintessential app description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your quintessential contact page.";
-
-            return View();
-        }
+        }        
     }
 }
